e3ca883b232a13bff1a33a243925e552991fa4da
Daniel Hartnell <danielhartnell@gmail.com>
2017-04-13 20:57:15 -0700

    corrected indentation

--- golang-book/chapter5/range_loop.go
+++ golang-book/chapter5/range_loop.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-				var x [3]int
-				x[0] = 2
-				x[1] = 4
-				x[2] = 6
+  var x [3]int
+  x[0] = 2
+  x[1] = 4
+  x[2] = 6
 
-				for _, value := range x {
-								fmt.Println(value)
-				}
+  for _, value := range x {
+    fmt.Println(value)
+  }
 }
